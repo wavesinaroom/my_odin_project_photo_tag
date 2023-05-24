f7ed25791ba42756caeadbc192e50453fdfcaa2f
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-24 11:59:07 -0500

    [Task] Delete 'gets non zero user's click coordinates'. This test should be performed by picker.test.js

--- src/components/image.test.js
+++ src/components/image.test.js
@@ -1,19 +1,16 @@
 import React from "react";
-import {cleanup, render, screen} from "@testing-library/react";
+import {cleanup, fireEvent, render, screen} from "@testing-library/react";
 import Image from "./image";
 
 beforeEach(()=>{
   cleanup();
 });
 
-it.only(`renders an image`,()=>{
+it(`renders an image`,()=>{
   render(<Image/>);
   expect(screen.getByRole(`img`)).toBeInTheDocument();
 });
 
-it(`gets non zero user's click coordinates`,()=>{
-
-});
 
 it(`fetches item coordinates from server based on <Picker/> selection`,()=>{
 
