5b35225e53c9f87a09a4cda62e098a8d53b4b453
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-24 12:00:29 -0500

    [Fix] Add a reference to the event when creating the new click object inside handleUserClick function

--- src/components/image.js
+++ src/components/image.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 const Image = () =>{
+  const [click, setClick] = useState({x:0, y:0});
 
+  function handleUserClick(e){
+    const newClick = {x:e.clientX, y:e.clientY} 
+    setClick(newClick);
+  }
   return(
     <>
-      <img src="https://www.puzzleprime.com/wp-content/uploads/2018/10/bigstock-Find-Objects-Visual-Game-Solu-72484915-1-1920x1574.jpg"/>
+      <img src="https://www.puzzleprime.com/wp-content/uploads/2018/10/bigstock-Find-Objects-Visual-Game-Solu-72484915-1-1920x1574.jpg" onClick={handleUserClick}/>
     </>
   );
 }
