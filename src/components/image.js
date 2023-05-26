90205a2f6b365a49ed38d522d270385230f712cd
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-26 09:50:34 -0500

    [Task] Commit current code to avoid conflicts between it and the soon reopened branch 2-picker

--- src/components/image.js
+++ src/components/image.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from "react";
 import {styled} from 'styled-components';
-import {supase} form '../config/supabaseClient'
+import {supase} from '../config/supabaseClient'
 
-const Image = () =>{
+const Image = ({found}) =>{
   const [coordinate, setCoordinate] = useState(null);
   const [selection, setSelection] = useState(null);
   const [isOpened, toggleModal] = useState(false);
@@ -16,33 +16,43 @@ const Image = () =>{
   }
 
   function handleItemSelection(e){
-    setSelection({coordinates:{coordinate}, item:e.value});
+    setSelection({coordinate:{coordinate}, name:e.value});
     toggleModal(!isOpened);
+
+    const item = items.find(i => i.name === selection.item)
+    
+    if(selection.coordinate.x>item.left&&selection.coordinate.x<item.right)
+      if(selection.coordinate.y>item.bottom&&selection.coordinate.y<item.top)
+        setFound(true);
+    else
+        setFound(false);
   }
 
   useEffect(()=>{
     
-    const{data,error} = await supase
-      .from(`coordinates`)
-      .select();
+    const fetchOptions = async ()=>{
+      const{data,error} = await supase
+        .from(`coordinates`)
+        .select();
 
-      if(error){
-        setFetchError(error);
-        setItems(null);
-        console.log(error);
-      }
+        if(error){
+          setFetchError(error);
+          setItems(null);
+          console.log(error);
+        }
 
-      if(data){
-        setItems(data);
-        setFetchError(error);
-        console.dir(data);
+        if(data){
+          setItems(data);
+          setFetchError(error);
+          console.dir(data);
+        }
       }
   },[])
 
   return(
     <>
       <img alt="sarah's-mess" src="https://www.puzzleprime.com/wp-content/uploads/2018/10/bigstock-Find-Objects-Visual-Game-Solu-72484915-1-1920x1574.jpg" onClick={handleUserClick}/>
-      <dialog open={isOpened}>
+      <dialog open={isOpened} style={`top:${coordinate.y}; left:${coordinate.x}`}>
         <menu>
           <button onClick={handleItemSelection}>Cat</button>
           <button onClick={handleItemSelection}>Dog</button>
@@ -59,6 +69,4 @@ export default Image;
 
 const dialog = styled.dialog`
   position: absolute;
-  top: ${props=>props.coordinate.y}px;
-  left: ${props=>props.coordinate.x}px;
 `
