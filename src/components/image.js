8094b50c8f7f7053c079b1182de737a37015e550
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-29 16:54:34 -0500

    [Feature] Toogle dialog inside an image

--- src/components/image.js
+++ src/components/image.js
@@ -1,13 +1,29 @@
-import { useState } from "react";
-import Picker from "./picker";
+import { useState} from "react";
 
 const Image = () =>{
   const [click, setClick] = useState({x:0, y:0});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleModalPopup(e){
+    setClick({x:e.clientX, y:e.clientY});
+    setIsModalOpen(!isModalOpen);
+  }  
+  function handleSelection(){
+    setIsModalOpen(!isModalOpen)
+  }
 
   return(
     <>
-      <img alt="sarah's-mess" src="https://www.puzzleprime.com/wp-content/uploads/2018/10/bigstock-Find-Objects-Visual-Game-Solu-72484915-1-1920x1574.jpg" onClick={e=>{setClick({x:e.clientX, y:e.clientY})}} />
-      <Picker click={click}/>
+      <img alt="sarah's-mess" src="https://www.puzzleprime.com/wp-content/uploads/2018/10/bigstock-Find-Objects-Visual-Game-Solu-72484915-1-1920x1574.jpg" onClick={handleModalPopup} />
+      <dialog style={{position:'absolute', top: `${click.x}px`, left:`${click.y}px`}}open={isModalOpen}>
+        <menu>
+          <button onClick={handleSelection}>Cat</button>
+          <button onClick={handleSelection}>Dog</button>
+          <button onClick={handleSelection}>Crayons</button>
+          <button onClick={handleSelection}>Book</button>
+          <button onClick={handleSelection}>Doll</button>
+        </menu>
+      </dialog>
     </>
   );
 }
