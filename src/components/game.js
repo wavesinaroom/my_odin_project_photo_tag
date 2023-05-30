902c4d67c7165024299362628977a401b07a16f2
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-30 12:35:19 -0500

    [Task] Refactor game logic

--- src/components/game.js
+++ src/components/game.js
@@ -31,17 +31,12 @@ const Game = ()=>{
 
   useEffect(()=>{
     const item = items.find(item => item.name === selection.name )
-    let found = false;
 
     if(selection.click.x>item.left&&selection.click.x<item.right)
       if(selection.click.y>item.bottom&&selection.click.y<item.top)
-        found = true;
-
-    if(found){
-      items.filter(item=>item.name!==selection.name)
-      if(items.length===0)
-        setWin(!win);
-    }
+        items.filter(item=>item.name!==selection.name)
+        if(items.length===0)
+          setWin(!win);
 
   },[items,win,selection])
 
