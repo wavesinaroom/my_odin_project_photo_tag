920ab841ee6762fabfa15c1f5b5ad60f7d68fde1
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-30 11:23:14 -0500

    [Task] Update tests

---
+++ src/components/game.test.js
@@ -0,0 +1,26 @@
+import {cleanup} from "@testing-library/react";
+import React from "react";
+
+beforeEach(()=>{
+  cleanup();
+})
+
+it.skip(`pops up Ready? dialog`,()=>{
+
+});
+
+it.skip(`starts game on player confirmation`,()=>{
+
+});
+
+it.skip(`throws error on failure`, ()=>{
+
+});
+
+it.skip(`renders input on success`, ()=>{
+
+});
+
+it.skip(`resets states on game over`,()=>{
+
+});
