a722faf77116a0d877e7147e16f938f0d26dd7bd
wavesinaroom <jaureguij@javeriana.edu.co>
2023-05-31 10:22:29 -0500

    [Fix] Format url and key strings to create supabase client

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,9 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl ="https://uzyvgtamixvkwxeklhpz.supabase.co" 
-const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6eXZndGFtaXh2a3d4ZWtsaHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4Nzc4OTQsImV4cCI6MjAwMDQ1Mzg5NH0.MRwjrW03JISwsY0p4T_FrlsKzSi9tOzvoaVJjxOdXFk" 
+const supabaseUrl ="https://uzyvgtamixvkwxeklhpz.supabase.co";
+const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6eXZndGFtaXh2a3d4ZWtsaHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4Nzc4OTQsImV4cCI6MjAwMDQ1Mzg5NH0.MRwjrW03JISwsY0p4T_FrlsKzSi9tOzvoaVJjxOdXFk"; 
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+
 
