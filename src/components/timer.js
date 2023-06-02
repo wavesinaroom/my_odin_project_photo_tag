1239744cdb6fdef138a2b81f651038aa2c01ee45
wavesinaroom <jaureguij@javeriana.edu.co>
2023-06-02 15:51:43 -0500

    [Task] Format time to HH:MM:ss

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,18 +1,31 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const Timer = ({time, setTime}) =>{
-
+  const [format, setFormat] = useState("");
   useEffect(()=>{
+    
+    function formatTime(total){
+      let hours = Math.floor(total/3600);
+      let minutes = Math.floor(total/60); 
+      let seconds = total % 60;
+
+      hours = String(hours).padStart(2,"0");
+      minutes = String(minutes).padStart(2,"0");
+      seconds = String(seconds).padStart(2,"0");
+
+      return (`${hours}:${minutes}:${seconds}`);
+    }
     
     setTimeout(()=>{
       const update = time+1;
       setTime(update);
+      setFormat(formatTime(update));
     },1000)
       
   })
 
   return(
-    <p>{time}</p>
+    <p>{format}</p>
   )
 
 }
