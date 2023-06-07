87029920dd504559a7fc911465df678a7e103734
wavesinaroom <jaureguij@javeriana.edu.co>
2023-06-07 15:49:09 -0500

    [Task] Keep timer format consistent in leaderboard

--- src/components/record.js
+++ src/components/record.js
@@ -1,9 +1,25 @@
+import {useState} from "react";
+
 const Record = ({record}) =>{
+  const [formatted, setFormatted] = useState(formatTime(record.time));
+
+  function formatTime(total){
+    let hours = Math.floor(total/3600);
+    let minutes = Math.floor(total/60); 
+    let seconds = total % 60;
+
+    hours = String(hours).padStart(2,"0");
+    minutes = String(minutes).padStart(2,"0");
+    seconds = String(seconds).padStart(2,"0");
+
+    return (`${hours}:${minutes}:${seconds}`);
+  }
+
   return(
     <>
       <tr>
         <td>{record.username}</td>
-        <td>{record.time}</td>
+        <td>{formatted}</td>
       </tr>
     </>
   )
