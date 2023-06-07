a35b8fd7d3deed3c54a218a344c015a5ac104f5b
wavesinaroom <jaureguij@javeriana.edu.co>
2023-06-07 11:31:28 -0500

    [Task] Remove test files and other unnecessary stuff

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Routes, Route, BrowserRouter} from 'react-router-dom'
-import App from './App'
 import Game from './components/game'
 import LeadBoard from './components/leader-board'
+import Home from './components/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/game' element={<Game/>}/>
         <Route path='/leadboard' element={<LeadBoard/>}/>
       </Routes>
