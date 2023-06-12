e346fc95d46ecc9ac9dfa124b10ad644627e68dd
wavesinaroom <jaureguij@javeriana.edu.co>
2023-06-12 11:39:32 -0500

    [Feature] Home text box

--- src/components/home.js
+++ src/components/home.js
@@ -6,11 +6,11 @@ const Home = ()=>{
     <div style={divStyle}>
       <h1>Sarah&apos;s mess</h1>
       <p>Sarah wants to come out and play with her friends but she can&apos;t find her favourite toys to play with. If you help her out, she promises she will keep her room tidy and clean.</p>
-      <ul>
-        <li>Sarah will think about her toys while you look for them in the pile.</li>
-        <li>If you spot one of them, click on it and select its name from the list.</li>
-        <li>You must find Sarah&apos;s toys as quick as possible to rank high in the leaderboard.</li>
-        <li>Time will start ticking once you click on the button below</li>
+      <ul style={{listStyle:`none`}}>
+        <li style ={{paddingBottom:`1em`}}>Sarah will think about her toys while you look for them in the pile.</li>
+        <li style ={{paddingBottom:`1em`}}>If you spot one of them, click on it and select its name from the list.</li>
+        <li style = {{paddingBottom:`1em`}}>You must find Sarah&apos;s toys as quick as possible to rank high in the leaderboard.</li>
+        <li style = {{paddingBottom:`1em`}}>Time will start ticking once you click on the button below</li>
       </ul>
       <Link to='/game'><button>Let&apos;s go</button></Link>
     </div>
@@ -19,10 +19,19 @@ const Home = ()=>{
 export default Home;
 
 const divStyle = {
-  backgroundColor: `#EDE9CA`,
-  display: `flex`,
-  flexDirection: `column`,
-  alignItems: `center`,
-  position:`relative`,
-  top: `5vh`
+  backgroundColor:"#EDE9CA",
+  display:"flex",
+  flexDirection: "column",
+  alignItems:"center",
+  height: "450px",
+  width: "700px",
+  position: "absolute",
+  left:"55%", 
+  top: "40%",
+  marginLeft: "-250px",
+  marginTop: "-250px",
+  paddingLeft: "3em",
+  paddingRight: "3em",
+  borderRadius: "7px"
 }
+
